6b0378353e78c09b86b13285aa9b2d33d7319171
李嘉豪 <lijiahao8350@cvte.com>
2021-09-30 15:39:44 +0800

    [bugfix][v1.0.0]将一些功能移动到MainActivity
    [what]将一些功能移动到MainActivity
    [why]HomeFragment过于复杂
    [how]将一些功能移动到MainActivity
    
    [Famous]

--- app/src/main/java/com/ba/ex/mvvmsample/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ba/ex/mvvmsample/ui/activity/MainActivity.kt
@@ -3,14 +3,32 @@ package com.ba.ex.mvvmsample.ui.activity
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.app.Dialog
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.findNavController
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.ba.ex.mvvmsample.R
 import com.ba.ex.mvvmsample.databinding.ActivityMainBinding
+import com.ba.ex.mvvmsample.log.workers.ArtificialUploadLogWorker
+import com.ba.ex.mvvmsample.ui.fragment.viewmodels.LoggerInfoViewModel
+import com.ba.ex.mvvmsample.ui.fragment.viewmodels.MsgViewModel
+import com.orhanobut.logger.Logger
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -18,13 +36,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         const val PERMISSIONS_CODE = 1002
     }
 
+    private var receiveMsgStart = false
+    private var confirmDialog: Dialog? = null //删除dialog
+    private val loggerInfoViewModel: LoggerInfoViewModel by viewModels()
+    private val msgViewModel: MsgViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestPermissions()
     }
 
     override fun onBinding(savedInstanceState: Bundle?): ActivityMainBinding =
-        DataBindingUtil.setContentView(this,
+        DataBindingUtil.setContentView(
+            this,
             R.layout.activity_main
         )
 
@@ -87,5 +111,82 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                     }
                 }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_home_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_like -> {
+                getDataBinding().navHost.findNavController()
+                    .navigate(R.id.home_fragment_to_like_fruit_fragment)
+
+                true
+            }
+            R.id.menu_upload_logger -> {
+                showUpLoadLogDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        receiveMsg(menu)
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    private fun receiveMsg(menu: Menu) {
+        //防止重复创建job
+        if (receiveMsgStart) {
+            return
+        }
+
+        lifecycleScope.launch {
+            val msg = getString(R.string.menu_msg)
+            receiveMsgStart = true
+            //只有在数据有变化时，且生命周期处与STARTED后才能收到，适合用来做实时状态监听的视图
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                msgViewModel.msgNum.collectLatest {
+                    Logger.d(">>> onPrepareOptionsMenu $it")
+                    //item加载可能会慢一点，所以加个判空检测
+                    while (menu.findItem(R.id.menu_msg) == null) {
+                        delay(500)
+                    }
+                    menu.findItem(R.id.menu_msg).title = java.lang.String.format(msg, it)
+                }
+            }
+            receiveMsgStart = false
+        }
+    }
+
+    private fun showUpLoadLogDialog() {
+        var num = 0
+        loggerInfoViewModel.logInfo.value?.let {
+            num = it.size
+        }
+        //日志数量
+        confirmDialog = AlertDialog.Builder(this)
+            .setTitle(getString(R.string.upload_log_dialog_title))
+            .setMessage("${getString(R.string.upload_log_dialog_message)} : $num")
+            .setPositiveButton(getString(R.string.dialog_ok)) { dialog, which ->
+                val request = OneTimeWorkRequestBuilder<ArtificialUploadLogWorker>().build()
+                WorkManager.getInstance(this).enqueue(request)
+                Toast.makeText(
+                    this,
+                    getString(R.string.toast_upload_loginfo),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setNegativeButton(getString(R.string.dialog_cancel)) { dialog, which ->
+            }.create().apply { show() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        confirmDialog?.cancel()
     }
 }
--- app/src/main/java/com/ba/ex/mvvmsample/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/ba/ex/mvvmsample/ui/fragment/HomeFragment.kt
@@ -1,45 +1,27 @@
 package com.ba.ex.mvvmsample.ui.fragment
 
-import android.app.Dialog
 import android.os.Bundle
-import android.view.*
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import com.ba.ex.mvvmsample.R
 import com.ba.ex.mvvmsample.databinding.FragmentHomeBinding
-import com.ba.ex.mvvmsample.log.workers.ArtificialUploadLogWorker
 import com.ba.ex.mvvmsample.ui.fragment.viewmodels.FruitsViewModel
-import com.ba.ex.mvvmsample.ui.fragment.viewmodels.LoggerInfoViewModel
-import com.ba.ex.mvvmsample.ui.fragment.viewmodels.MsgViewModel
 import com.ba.ex.mvvmsample.ui.recycler.adapter.FruitListAdapter
 import com.ba.ex.mvvmsample.ui.views.LoadingDialog
 import com.orhanobut.logger.Logger
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import java.lang.String.format
 
 /**
  * 水果列表和水果详情的展示
  */
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private lateinit var adapter: FruitListAdapter
-    private var confirmDialog: Dialog? = null //删除dialog
     private var loadingDialog: LoadingDialog? = null     //加载dialog
-    private var receiveMsgStart = false
-
     private val fruitsViewModel: FruitsViewModel by viewModels()
-    private val loggerInfoViewModel: LoggerInfoViewModel by viewModels()
-    private val msgViewModel: MsgViewModel by viewModels()
 
     override fun onBinding(
         inflater: LayoutInflater,
@@ -111,80 +93,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_home_list, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.menu_like -> {
-                NavHostFragment.findNavController(this)
-                    .navigate(R.id.home_fragment_to_like_fruit_fragment)
-                true
-            }
-            R.id.menu_upload_logger -> {
-                showUpLoadLogDialog()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        super.onPrepareOptionsMenu(menu)
-        receiveMsg(menu)
-    }
-
-    private fun receiveMsg(menu: Menu) {
-        //防止重复创建job
-        if (receiveMsgStart) {
-            return
-        }
-
-        lifecycleScope.launch {
-            val msg = getString(R.string.menu_msg)
-            receiveMsgStart = true
-            //只有在数据有变化时，且生命周期处与STARTED后才能收到，适合用来做实时状态监听的视图
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                msgViewModel.msgNum.collectLatest {
-                    Logger.d(">>> onPrepareOptionsMenu $it")
-                    //item加载可能会慢一点，所以加个判空检测
-                    while (menu.findItem(R.id.menu_msg) == null) {
-                        delay(500)
-                    }
-                    menu.findItem(R.id.menu_msg).title = format(msg, it)
-                }
-            }
-            receiveMsgStart = false
-        }
-    }
-
-    private fun showUpLoadLogDialog() {
-        var num = 0
-        loggerInfoViewModel.logInfo.value?.let {
-            num = it.size
-        }
-        //日志数量
-        confirmDialog = AlertDialog.Builder(requireContext())
-            .setTitle(getString(R.string.upload_log_dialog_title))
-            .setMessage("${getString(R.string.upload_log_dialog_message)} : $num")
-            .setPositiveButton(getString(R.string.dialog_ok)) { dialog, which ->
-                val request = OneTimeWorkRequestBuilder<ArtificialUploadLogWorker>().build()
-                WorkManager.getInstance(requireContext()).enqueue(request)
-                Toast.makeText(
-                    requireContext(),
-                    getString(R.string.toast_upload_loginfo),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            .setNegativeButton(getString(R.string.dialog_cancel)) { dialog, which ->
-            }.create().apply { show() }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         loadingDialog?.cancel()
-        confirmDialog?.cancel()
     }
 
 }
